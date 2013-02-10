86d9950a3904493dd0df1baf4a9e28681e3db5b1
Chad Woods <woods.chad@gmail.com>
2013-02-10 04:02:33 -0500

    Add Model with Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Black::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  #check the other page
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
