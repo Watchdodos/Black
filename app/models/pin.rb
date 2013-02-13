055287adfe10257178f2e520a466b628535325e3
Chad Woods <woods.chad@gmail.com>
2013-02-13 03:23:22 -0500

    Add Pins Associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,7 @@ class Pin < ActiveRecord::Base
 
   validates :description, presence: true, :length => { :maximum => 140,
     :too_long => "%{count} characters is the maximum allowed" }
+
+    belongs_to :user
+    validates :user_id, presence: true
 end
