0add2f329dabe6b55530fa61f82fd09c24be5d23
Chad Woods <woods.chad@gmail.com>
2013-04-04 00:59:48 -0400

    debugging8

--- config/initializers/wuparty.rb
+++ config/initializers/wuparty.rb
@@ -1,5 +1,5 @@
 require 'wuparty'
 
-WUFOO_ACCOUNT = 'chadwoods'
-WUFOO_API_KEY = 'GTLI-A1D7-O02X-O5FI'
+WUFOO_ACCOUNT = 'colorpledge'
+WUFOO_API_KEY = 'E5MP-FTLN-QG7I-2LZK'
 WUFOO_FORM_ID = 'z7x3p3'
